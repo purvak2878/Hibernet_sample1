8c8ef8375a3c9b4aab1ef536d47d6b798774e815
purvak2878 <purvakulkarni456@gmail.com>
2026-07-01 19:15:55 +0530

    Initial commit

---
+++ src/main/java/com/ssit/Hibernet_sample/HibernetSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ssit.Hibernet_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernetSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernetSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssit/Hibernet_sample/HibernetSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssit.Hibernet_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernetSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
